44b2b746e67351ecab5d3afb321be743fc3da362
DieReiterin <zzerafim@ya.ru>
2023-06-13 13:35:35 +0300

    Added untracked files so the app actually works 1.1.0

---
+++ src/components/AppLights.vue
@@ -0,0 +1,73 @@
+<template>
+    <ul class="lights__list">
+      <li 
+      :class="{
+        'active': $store.state.dimLights === false && color === 'red'
+      }"
+      class="lights__item lights__item-first"></li>
+      <li 
+      :class="{
+        'active': $store.state.dimLights === false && color === 'yellow'
+      }" 
+      class="lights__item lights__item-sec"></li>
+      <li 
+      :class="{
+        'active': $store.state.dimLights === false && color === 'green'
+      }" 
+      class="lights__item lights__item-third"></li>
+    </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: "none",
+    }
+  }
+}
+</script>
+
+<style scoped>
+.lights__list {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 150px;
+  height: 400px;
+  margin: auto;
+  border-radius: 25px;
+  background-color: #000;
+  list-style: none;
+}
+.lights__item {
+  position: relative;
+  width: 100px;
+  height: 100px;
+  margin: 15px;
+  border-radius: 50%;
+}
+.lights__item-first {
+  background-color: red;
+}
+.lights__item-sec {
+  background-color: yellow;
+}
+.lights__item-third {
+  background-color: lawngreen;
+}
+.lights__item::after {
+  content: '';
+  position: absolute;
+  top: center;
+  right: center;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background-color: rgba(0, 0, 0, 0.8);
+}
+.lights__item.active::after {
+  background-color: rgba(0, 0, 0, 0);
+}
+</style>
---
+++ src/pages/YellowPage.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- <h1>This is YELLOW page</h1> -->
+    <div class="container">
+      <app-lights
+      :color=pageColor
+      ></app-lights>
+    </div>
+</template>
+
+<script>
+import AppLights from '@/components/AppLights.vue'
+
+export default {
+  components: {
+    AppLights
+  },
+  data() {
+    return {
+      pageColor: "yellow",
+    };
+  },
+}
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  width: 100%;
+  min-height: 600px;
+  margin: 0 auto;
+  padding: 35px 50px 100px;
+  background-color: slategray;
+}
+</style>
